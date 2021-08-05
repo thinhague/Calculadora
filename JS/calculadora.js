b0e271eb492cf692ed62a014e89bcdb8d101ebf5
Thinhague <thinhague@outlook.com>
2021-08-05 19:42:46 -0300

    attFinal

---
+++ JS/calculadora.js
@@ -0,0 +1,39 @@
+
+function numero(num){
+    let numero = document.getElementById("resultado").innerHTML;
+    if(numero == 0){
+        
+        document.getElementById("resultado").innerHTML = num;
+    }else{
+        document.getElementById("resultado").innerHTML = numero + num;
+    
+     }
+   
+    
+    
+}
+
+function clean(){
+     document.getElementById("resultado").innerHTML = '';
+    
+}
+function calcular(){
+    let numero = document.getElementById("resultado").innerHTML;
+    if(numero){
+        document.getElementById("resultado").innerHTML = eval(numero)
+    } 
+}
+function operator(){
+    let numero = document.getElementById("resultado").innerHTML;
+    let negativo = numero * -1;
+    
+    if(numero > 0 || numero < 0){
+        document.getElementById("resultado").innerHTML = negativo;
+    }
+
+
+
+    
+}
+
+
